f06fe417c7ee40afdfab6d406c717b02e18f0284
Breyner012 <bhernandezos@cesde.net>
2024-10-10 13:53:56 -0500

    Primer commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// console.log("Hola Mundo") 
+
+// prompt("Ingrese Cualquier Cosa")
+
+// Variables
+
+// var insulto = "Cara Economica"
+// var nombre = prompt("Ingrese Nombre: ")
+// console.log(nombre+" "+insulto)
+// prompt () siempre va a ser String 
+
+// var num1 = parseInt( prompt("Ingresa El Primer Numero")) /*Elimina las pocisiones desimales */
+// var num2 = Number( prompt("Ingresa El Segundo Numero"))  /*Acepta valores enteros y desimales igual que el parseFloat*/
+// var resultado= num1+num2
+// console.log(resultado)
+
+// console.log("2"+2+"2")
+// console.log(("2"+2)+"2")
+// console.log(("2"+2)-"2")
+// console.log("2"+"2"-"2")
+
